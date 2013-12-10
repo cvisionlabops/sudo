6c81ffa09aef465d4bcf333bb80db2b9c9f4b126
cvl-skubriev <skubriev@cvisionlab.com>
2013-12-10 11:43:13 +0300

    Unhardcode ['authorization']['sudo']['package_name']

--- attributes/default.rb
+++ attributes/default.rb
@@ -23,6 +23,7 @@ default['authorization']['sudo']['passwordless']      = false
 default['authorization']['sudo']['include_sudoers_d'] = false
 default['authorization']['sudo']['agent_forwarding']  = false
 default['authorization']['sudo']['sudoers_defaults']  = ['!lecture,tty_tickets,!fqdn']
+default['authorization']['sudo']['package_name']            = "sudo"
 
 case node['platform_family']
 when 'smartos'
